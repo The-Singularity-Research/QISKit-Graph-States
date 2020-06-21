0696968600c0f67503b67209451f51b234e6e362
Jeremy Langley <jlangley2code@gmail.com>
2020-06-21 00:59:53 -0700

    consolidate __init__ and build_node_dict()

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="QISKit-Graph-States",  # Replace with your own username
-    version="0.0.7",
+    version="0.0.8",
     author="Amelie Schreiber",
     author_email="thesingularity.research@gmail.com",
     description="Graph states for MBQC, error correction, and quantum cryptography",
